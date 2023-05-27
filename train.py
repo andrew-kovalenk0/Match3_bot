690a9b20ccdc9815e1dc8f55de78bf6b580cda2d
Andrei Kavalenka <andrew.kovalenko65@gmail.com>
2023-05-27 11:27:15 +0200

    Remove cross_entropy_loss func, add parquet dependencies to requirements.txt

--- train.py
+++ train.py
@@ -29,20 +29,17 @@ class LightningMNISTClassifier(pl.LightningModule):
 
         return x
 
-    def cross_entropy_loss(self, logits, labels):
-        return f.nll_loss(logits, labels)
-
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
         logits = self.forward(x)
-        loss = self.cross_entropy_loss(logits, y)
+        loss = f.nll_loss(logits, y)
         self.log('train_loss', loss)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
         logits = self.forward(x)
-        loss = self.cross_entropy_loss(logits, y)
+        loss = f.nll_loss(logits, y)
         self.log('val_loss', loss)
 
     def configure_optimizers(self):
